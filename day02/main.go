3545a63dd2fd227e597dc0e95b854a2c79261d14
arthurdepina <arthurdepinabalduinoleitao@gmail.com>
2024-12-03 19:51:35 -0300

    day 2 pt 2

--- day02/main.go
+++ day02/main.go
@@ -18,7 +18,13 @@ func InValidRange(n int) bool {
 	return -3 <= n && n <= 3 && n != 0
 }
 
-func CountSafeReports(path string) int {
+func RemoveFromReport(slice []int, i int) []int {
+	result := make([]int, len(slice))
+	copy(result, slice)
+	return append(result[:i], result[i+1:]...)
+}
+
+func CountSafeReports(path string) (int, int) {
 	file, err := os.Open("input/day_2.txt")
 	check(err)
 	defer file.Close()
@@ -26,6 +32,7 @@ func CountSafeReports(path string) int {
 	scanner := bufio.NewScanner(file)
 
 	countSafe := 0
+	countSafeTolerant := 0
 	for scanner.Scan() {
 		var report []int
 		line := strings.Split(scanner.Text(), " ")
@@ -36,8 +43,13 @@ func CountSafeReports(path string) int {
 		if ValidateReport(report) {
 			countSafe++
 		}
+		if ValidateReportTolerant(report) {
+			countSafeTolerant++
+		} else {
+		}
+
 	}
-	return countSafe
+	return countSafe, countSafeTolerant
 }
 
 func ValidateReport(report []int) bool {
@@ -65,6 +77,42 @@ func ValidateReport(report []int) bool {
 	return true
 }
 
+func ValidateReportTolerant(report []int) bool {
+	if len(report) <= 2 {
+		return InValidRange(report[0] - report[1])
+	}
+	if (report[0] < report[1] && report[1] > report[2]) || (report[0] > report[1] && report[1] < report[2]) {
+		report_a := RemoveFromReport(report, 0)
+		if ValidateReport(report_a) {
+			return true
+		}
+	}
+
+	i := 1
+	if report[i] > report[i-1] { // increasing
+		for i < len(report) {
+			if report[i] < report[i-1] || !InValidRange(report[i]-report[i-1]) {
+				report_a := RemoveFromReport(report, i)
+				report_b := RemoveFromReport(report, i-1)
+				return ValidateReport(report_a) || ValidateReport(report_b)
+			}
+			i++
+		}
+	} else { // decreassing
+		for i < len(report) {
+			if report[i] > report[i-1] || !InValidRange(report[i]-report[i-1]) {
+				report_a := RemoveFromReport(report, i)
+				report_b := RemoveFromReport(report, i-1)
+				return ValidateReport(report_a) || ValidateReport(report_b)
+			}
+			i++
+		}
+	}
+	return true
+}
+
 func main() {
-	fmt.Println("# safe reports: ", CountSafeReports("input/day_2.txt"))
+	safeReportCount, safeReportTolerantCount := CountSafeReports("input/day_2.txt")
+	fmt.Println("# safe reports: ", safeReportCount)
+	fmt.Println("# safe reports with tolerance: ", safeReportTolerantCount)
 }
