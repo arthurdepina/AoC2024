13d5ae7735a7bd2b51ff15f3dc675b3d5dc9fe8c
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-12-03 23:58:50 -0300

    day 3 pt 2

--- day03/main.go
+++ day03/main.go
@@ -36,7 +36,39 @@ func AddMults(path string) int {
 	return sum
 }
 
+func AddMultsConditional(path string) int {
+	sum := 0
+	file, err := os.Open(path)
+	check(err)
+	defer file.Close()
+
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)`)
+	add := true
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches := re.FindAllStringSubmatch(line, -1)
+		for _, match := range matches {
+			if match[0] == "don't()" {
+				add = false
+			}
+			if match[0] == "do()" {
+				add = true
+			}
+			if add {
+				num1, _ := strconv.Atoi(match[1])
+				num2, _ := strconv.Atoi(match[2])
+				sum += num1 * num2
+			}
+		}
+	}
+	return sum
+}
+
 func main() {
 	sum := AddMults("input/day_3.txt")
 	fmt.Println(sum)
+	conditionalSum := AddMultsConditional("input/day_3.txt")
+	fmt.Println(conditionalSum)
 }
